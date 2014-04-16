e11f29ff2225058c16b2f452c2fc39b64f72cd74
bkis <boerge.kiss@gmail.com>
2014-04-16 20:33:12 +0100

    roughly prototyped world and character physics/controls classes, some refactoring

--- src/pt/edj/cp/app/IngameState.java
+++ src/pt/edj/cp/app/IngameState.java
@@ -1,32 +1,47 @@
 package pt.edj.cp.app;
 
 import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
 import com.jme3.app.state.AbstractAppState;
 import com.jme3.app.state.AppStateManager;
+import com.jme3.scene.Node;
 import pt.edj.cp.input.IngameInputsState;
-import pt.edj.cp.physics.WorldPhysicsState;
+import pt.edj.cp.physics.WorldPhysicsManager;
 import pt.edj.cp.world.PlatformLifecycleManager;
 
 
 public class IngameState extends AbstractAppState{
     
+    private static final String CHAR_MODEL = "Models/Oto/Oto.j3m";
+    
+    private SimpleApplication app;
     private PlatformLifecycleManager lifecycleManager;
     
     private IngameInputsState ingameInputState;
-    private WorldPhysicsState worldPhysicsState;
+    private WorldPhysicsManager worldPhysicsManager;
+    
+    private Node sceneNode;
+    private Node character;
     
     
     
     @Override
     public void initialize(AppStateManager stateManager, Application app) {
+        this.app = (SimpleApplication) app;
         this.lifecycleManager = new PlatformLifecycleManager();
         
         //initialize world
         constructWorld();
         
-        //attach additional states
+        //initialize character
+        character = loadCharacterModel();
+        this.app.getRootNode().attachChild(character);
+        
+        //load and apply physics
+        worldPhysicsManager = new WorldPhysicsManager(app, character, sceneNode);
+        
+        //attach input state
         stateManager.attach(ingameInputState = new IngameInputsState());
-        stateManager.attach(worldPhysicsState = new WorldPhysicsState());
     }
     
     
@@ -43,7 +58,15 @@ public class IngameState extends AbstractAppState{
     
     
     private void constructWorld(){
+        sceneNode = new Node();
+        app.getRootNode().attachChild(sceneNode);
+        
         //TODO
+    }
+    
+    
+    private Node loadCharacterModel(){
+        return (Node) app.getAssetManager().loadModel(CHAR_MODEL);
     }
     
 }
