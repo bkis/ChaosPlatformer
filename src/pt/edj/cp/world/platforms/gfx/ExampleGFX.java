3935553ca6600234e9d475576882d227c5d50b25
bkis <boerge.kiss@gmail.com>
2014-04-19 16:56:52 +0100

    added delayed platform highlighting and GFX to AbstractPlatformGFX class (yeah!)

--- src/pt/edj/cp/world/platforms/gfx/ExampleGFX.java
+++ src/pt/edj/cp/world/platforms/gfx/ExampleGFX.java
@@ -6,49 +6,67 @@ import com.jme3.effect.ParticleMesh;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
 
 
 public class ExampleGFX extends AbstractPlatformGFX {
 
-    ParticleEmitter fireEffect;
+    private ParticleEmitter throwEffect;
+    private Material matGfx;
+    private Material matDefault;
             
     
     public ExampleGFX(Application app){
-        super(app);
+        super(app, 0.2f);
     }
     
     
     @Override
     public void playGFX() {
-        fireEffect.setParticlesPerSec(5);
-        fireEffect.emitAllParticles();
-        fireEffect.setParticlesPerSec(0);
+        //particles
+        throwEffect.setParticlesPerSec(20);
+        
+        //platform color
+        ((Geometry)spatial).setMaterial(matGfx);
+    }
+    
+    
+    @Override
+    public void stopGFX() {
+        //particles
+        throwEffect.setParticlesPerSec(0);
+        
+        //platform color
+        ((Geometry)spatial).setMaterial(matDefault);
     }
 
     
     @Override
     public void initializeGFX() {
-        fireEffect = new ParticleEmitter("Emitter", ParticleMesh.Type.Triangle, 10);
+        //particles
+        throwEffect = new ParticleEmitter("Emitter", ParticleMesh.Type.Triangle, 20);
         Material fireMat = new Material(app.getAssetManager(), "Common/MatDefs/Misc/Particle.j3md");
-        fireEffect.setMaterial(fireMat);
-        fireEffect.setImagesX(2); fireEffect.setImagesY(2); // 2x2 texture animation
-        fireEffect.setEndColor( new ColorRGBA(1f, 0f, 0f, 0f) );   // red
-        fireEffect.setStartColor( new ColorRGBA(1f, 1f, 0f, 1f) ); // yellow
-        fireEffect.getParticleInfluencer().setInitialVelocity(new Vector3f(0, 8, 0));
-        fireEffect.setStartSize(0.1f);
-        fireEffect.setEndSize(0.4f);
-        fireEffect.setGravity(0f,30f,0f);
-        fireEffect.setLowLife(0.5f);
-        fireEffect.setHighLife(0.5f);
-        fireEffect.getParticleInfluencer().setVelocityVariation(0.3f);
-        fireEffect.setParticlesPerSec(0);
+        throwEffect.setMaterial(fireMat);
+        throwEffect.setImagesX(2); throwEffect.setImagesY(2); // 2x2 texture animation
+        throwEffect.setEndColor( new ColorRGBA(1f, 0f, 0f, 0f) );   // red
+        throwEffect.setStartColor( new ColorRGBA(1f, 1f, 0f, 1f) ); // yellow
+        throwEffect.getParticleInfluencer().setInitialVelocity(new Vector3f(0, 8, 0));
+        throwEffect.setStartSize(0.1f);
+        throwEffect.setEndSize(0.4f);
+        throwEffect.setGravity(0f,30f,0f);
+        throwEffect.setLowLife(0.5f);
+        throwEffect.setHighLife(0.5f);
+        throwEffect.getParticleInfluencer().setVelocityVariation(0.3f);
+        throwEffect.setParticlesPerSec(0);
+        throwEffect.setLocalTranslation(spatial.getLocalTranslation());
+        spatial.getParent().attachChild(throwEffect);
         
-        fireEffect.setLocalTranslation(spatial.getLocalTranslation());
-        spatial.getParent().attachChild(fireEffect);
+        //platform colors
+        matDefault = ((Geometry)spatial).getMaterial().clone();
+        matGfx = ((Geometry)spatial).getMaterial().clone();
+        matGfx.setColor("Diffuse", ColorRGBA.White);
+        matGfx.setColor("Ambient", ColorRGBA.Yellow);
+        matGfx.setBoolean("UseMaterialColors", true);
     }
-    
-    
-    
 
-    
 }
