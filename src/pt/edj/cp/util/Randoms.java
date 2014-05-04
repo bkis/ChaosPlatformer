8eb1e9e186412a5e0cfc5ccb2c6a71a4d0d2148d
bkis <boerge.kiss@gmail.com>
2014-05-04 15:32:56 +0100

    new features in Randoms

--- src/pt/edj/cp/util/Randoms.java
+++ src/pt/edj/cp/util/Randoms.java
@@ -6,11 +6,22 @@ import com.jme3.math.ColorRGBA;
 public class Randoms {
     
     
+    public static ColorRGBA rndColorRGBA(float brightness, float alpha){
+        brightness = brightness % 1;
+        
+        return new ColorRGBA(
+                rndFloat(brightness, 1.0f),
+                rndFloat(brightness, 1.0f),
+                rndFloat(brightness, 1.0f),
+                alpha);
+    }
+    
+    
     public static ColorRGBA rndColorRGBA(float alpha){
         return new ColorRGBA(
-                rndFloat(0.2f, 1.0f),
-                rndFloat(0.2f, 1.0f),
-                rndFloat(0.2f, 1.0f),
+                rndFloat(0.0f, 1.0f),
+                rndFloat(0.0f, 1.0f),
+                rndFloat(0.0f, 1.0f),
                 alpha);
     }
     
