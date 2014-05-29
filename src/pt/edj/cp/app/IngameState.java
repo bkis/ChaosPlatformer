f61245b29210faeb9be8ef7dcc1d135f22ca6792
bkis <boerge.kiss@gmail.com>
2014-05-29 18:48:27 +0100

    increased camera distance/field of view

--- src/pt/edj/cp/app/IngameState.java
+++ src/pt/edj/cp/app/IngameState.java
@@ -39,7 +39,7 @@ public class IngameState extends AbstractAppState {
     //private static final String CHAR_MODEL = "Models/Character/Layer_0.001.mesh.j3o";
     private static final String CHAR_MODEL = "Models/Oto/Oto.mesh.j3o";
     private static final float CAM_Y_OFFSET = 1f;
-    private static final float CAM_Z_OFFSET = 18f; //18
+    private static final float CAM_Z_OFFSET = 25f; //18
     private static final float BG_SIZE_X = 24;
     private static final float BG_SIZE_Y = 20;
     
