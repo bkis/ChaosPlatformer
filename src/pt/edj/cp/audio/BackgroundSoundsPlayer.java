87f4daf46be110a04c1cbc1e976d087d16d90e1b
bkis <boerge.kiss@gmail.com>
2014-05-31 17:37:23 +0100

    adjusted pad playback parameters

--- src/pt/edj/cp/audio/BackgroundSoundsPlayer.java
+++ src/pt/edj/cp/audio/BackgroundSoundsPlayer.java
@@ -21,7 +21,7 @@ public class BackgroundSoundsPlayer implements IEventListener{
     private static final int PLAY_PAD_EVERY_X_BARS = 1;
     
     private static final float AMBIENT_VOLUME = 0.4f;
-    private static final float PAD_VOLUME     = 0.8f;
+    private static final float PAD_VOLUME     = 0.4f;
 
     
     private SoundPathManager sam;
